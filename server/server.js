a63d4d84848c70956b7dc0bebd932b68b73af6b5
John Baldwin <johnbaldwin82@gmail.com>
2023-08-10 22:53:46 -0600

    reconfigure app.js

--- server/server.js
+++ server/server.js
@@ -39,8 +39,7 @@ const startApolloServer = async (typeDefs, resolvers) => {
   db.once("open", () => {
     app.listen(PORT, () => {
       console.log(`Server Now running on port:${PORT}`);
-      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
-      );
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
     });
   });
 };
