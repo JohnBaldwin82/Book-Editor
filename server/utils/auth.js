19cb4d9133b965b9e30eac2243ece7fcf66c0726
John Baldwin <johnbaldwin82@gmail.com>
2023-08-15 10:57:56 -0600

    make it better

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -9,7 +9,7 @@ module.exports = {
     // allows token to be sent via req.body, req.query, or headers
     let token = req.body.token || req.query.token || req.headers.authorization;
 
-    // separate ""Bearer"" from ""[tokenvalue]"
+    // separate ""Bearer"" from ""[tokenvalue]".
     if (req.headers.authorization) {
       token = token.split(' ').pop().trim();
     }
